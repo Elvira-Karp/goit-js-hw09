811b96c0f09ba187af4b65b4a0c69f38af75b373
Elvira-Karp <125654804+Elvira-Karp@users.noreply.github.com>
2023-09-07 11:24:24 +0300

    add

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,98 +1,46 @@
 import flatpickr from 'flatpickr';
-import convertMs from './dateConvert';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-// Import additional css styles
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
-// Get element date input, start btn, data: days, hours, min, sec
-let getRef = selector => document.querySelector(selector);
-const imputDatePickerRef = getRef('#datetime-picker');
-const btnStartRef = getRef('[data-start]');
-const daysRef = getRef('[data-days]');
-const hoursRef = getRef('[data-hours]');
-const minutesRef = getRef('[data-minutes]');
-const secondsRef = getRef('[data-seconds]');
+const input = document.querySelector('#datetime-picker');
 
-// Set initial value
-let timeDifference = 0;
-let timerId = null;
-let formatDate = null;
+const startBtn = document.querySelector('button[data-start]');
 
-const options = {
+let timerDays = document.querySelector('span[data-days]');
+let timerHours = document.querySelector('span[data-hours]');
+let timerMinutes = document.querySelector('span[data-minutes]');
+let timerSeconds = document.querySelector('span[data-seconds]');
+
+const currentDate = new Date();
+
+startBtn.disabled = true;
+let timerId;
+
+const options = flatpickr(input, {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    currentDifferenceDate(selectedDates[0]);
+    if (selectedDates[0].getTime() - currentDate.getTime() < 0) {
+      Notiflix.Report.warning('WARNING!', 'Please choose a date in the future', 'Ok');
+    } else {
+      startBtn.disabled = false;
+      startBtn.addEventListener('click', () => {
+       const timerId = setInterval(() => {
+         const currentTime = new Date();
+         const ms = selectedDates[0].getTime() - currentTime.getTime();
+         timerDays.textContent = addLeadingZero(convertMs(ms).days);
+         timerHours.textContent = addLeadingZero(convertMs(ms).hours);
+         timerMinutes.textContent = addLeadingZero(convertMs(ms).minutes);
+         timerSeconds.textContent = addLeadingZero(convertMs(ms).seconds);
+
+         if (ms < 0) {
+           clearInterval(timerId);
+         }
+        }, 1000)
+      })
+    }
   },
-};
-
-btnStartRef.setAttribute('disabled', true);
-
-// Initial flatpickr
-flatpickr(imputDatePickerRef, options);
-
-// Set click event listener on button start
-btnStartRef.addEventListener('click', onBtnStart);
-// Reset timer on btn
-window.addEventListener('keydown', e => {
-  if (e.code === 'Escape' && timerId) {
-    clearInterval(timerId);
-
-    imputDatePickerRef.removeAttribute('disabled');
-    btnStartRef.setAttribute('disabled', true);
-
-    secondsRef.textContent = '00';
-    minutesRef.textContent = '00';
-    hoursRef.textContent = '00';
-    daysRef.textContent = '00';
-  }
 });
 
-// Start timer
-function onBtnStart() {
-  timerId = setInterval(startTimer, 1000);
-}
-
-//date checking and rendering of date difference
-function currentDifferenceDate(selectedDates) {
-  const currentDate = Date.now();
-
-  if (selectedDates < currentDate) {
-    btnStartRef.setAttribute('disabled', true);
-    return Notify.failure('Please choose a date in the future');
-  }
-
-  timeDifference = selectedDates.getTime() - currentDate;
-  formatDate = convertMs(timeDifference);
-
-  renderDate(formatDate);
-  btnStartRef.removeAttribute('disabled');
-}
-
-//Timer
-function startTimer() {
-  btnStartRef.setAttribute('disabled', true);
-  imputDatePickerRef.setAttribute('disabled', true);
-
-  timeDifference -= 1000;
-
-  if (secondsRef.textContent <= 0 && minutesRef.textContent <= 0) {
-    Notify.success('Time end');
-    clearInterval(timerId);
-  } else {
-    formatDate = convertMs(timeDifference);
-    renderDate(formatDate);
-  }
-}
-
-// Rendering date
-function renderDate(formatDate) {
-  secondsRef.textContent = formatDate.seconds;
-  minutesRef.textContent = formatDate.minutes;
-  hoursRef.textContent = formatDate.hours;
-  daysRef.textContent = formatDate.days;
-}
